7f2effca6642c3ce4206040e965cceb9fde0dffa
yeyr2 <93815952+lql-arch@users.noreply.github.com>
2022-01-10 21:30:32 +0800

    Add files via upload

--- strbuf/strbuf2.c
+++ strbuf/strbuf2.c
@@ -29,26 +29,26 @@ void strbuf_add(struct strbuf *sb, const void *data, size_t len)
     sb->buf[sb->len]='\0';
 }
 
-void strbuf_addch(struct strbuf *sb, int c)
+void strbuf_addch(strbuf *sb, int c)
 {
     int l=sb->len;
     strbuf_grow(sb,1);
     sb->buf=(char*)realloc(sizeof(char)*sb->alloc+1);
-    sb->buf[l]=c;
+    sb->buf[l]=(char)c;
     sb->buf[sb->len]='\0';
 }
 
-void strbuf_addstr(struct strbuf *sb, const char *s)
+void strbuf_addstr(strbuf *sb, const char *s)
 {
     strbuf_add(sb,s,strlen(s));
 }
 
-void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2)
+void strbuf_addbuf(strbuf *sb, const struct strbuf *sb2)
 {
     strbuf_add(sb,sb2->buf,sb2->len);
 }  
 
-static inline void strbuf_setlen(struct strbuf *sb, size_t len)
+static inline void strbuf_setlen(strbuf *sb, size_t len)
 {
     sb->len=0;
     sb->alloc=0;
@@ -57,13 +57,13 @@ static inline void strbuf_setlen(struct strbuf *sb, size_t len)
     sb->buf[sb->len]='\0';
 }
 
-static inline size_t strbuf_avail(const struct strbuf *sb)
+static inline size_t strbuf_avail(const strbuf *sb)
 {
     size_t len = sb->alloc - sb->len;
     return len;
 }
 
-void strbuf_insert(struct strbuf *sb, size_t pos, const void *data, size_t len)
+void strbuf_insert(strbuf *sb, size_t pos, const void *data, size_t len)
 {  
     if (pos < sb->len)
     {
